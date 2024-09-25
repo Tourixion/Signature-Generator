7a59fdb3b6279ca50ed39466296fcc89c332bca4
Tourixion <marketing@tourix.gr>
2024-09-25 14:18:09 +0300

    Update email_signature_generator.py

--- email_signature_generator.py
+++ email_signature_generator.py
@@ -1,11 +1,12 @@
-import json
+import csv
+import io
 import requests
 from jinja2 import Template
 import os
 
 def get_gist_content(gist_id, filename):
-    github_token = os.environ['GITHUB_TOKEN']
-    headers = {'Authorization': f'token {github_token}'}
+    gist_token = os.environ['GIST_TOKEN']
+    headers = {'Authorization': f'token {gist_token}'}
     response = requests.get(f'https://api.github.com/gists/{gist_id}', headers=headers)
     response.raise_for_status()
     gist = response.json()
@@ -16,36 +17,39 @@ def load_template(gist_id, template_filename):
 
 def load_user_data(gist_id, user_data_filename):
     content = get_gist_content(gist_id, user_data_filename)
-    return json.loads(content)
+    csv_file = io.StringIO(content)
+    csv_reader = csv.DictReader(csv_file)
+    return list(csv_reader)
 
-def generate_signature(template_str, user_data):
+def generate_signatures(template_str, users_data):
     template = Template(template_str)
-    return template.render(user_data)
+    return [template.render(user) for user in users_data]
 
-def save_signature_to_gist(signature, gist_id, output_filename):
-    github_token = os.environ['GITHUB_TOKEN']
+def save_signatures_to_gist(signatures, gist_id, output_filename):
+    gist_token = os.environ['GIST_TOKEN']
     headers = {
-        'Authorization': f'token {github_token}',
+        'Authorization': f'token {gist_token}',
         'Accept': 'application/vnd.github.v3+json'
     }
+    content = "\n\n".join(signatures)
     data = {
         'files': {
             output_filename: {
-                'content': signature
+                'content': content
             }
         }
     }
     response = requests.patch(f'https://api.github.com/gists/{gist_id}', headers=headers, json=data)
     response.raise_for_status()
-    print(f"Email signature saved to Gist: {response.json()['html_url']}")
+    print(f"Email signatures saved to Gist: {response.json()['html_url']}")
 
 def main():
     gist_id = os.environ['GIST_ID']
     template = load_template(gist_id, 'email_signature_template.html')
-    user_data = load_user_data(gist_id, 'user_data.json')
-    signature = generate_signature(template, user_data)
-    save_signature_to_gist(signature, gist_id, 'generated_signature.html')
-    print("Email signature generated successfully!")
+    users_data = load_user_data(gist_id, 'user_data.csv')
+    signatures = generate_signatures(template, users_data)
+    save_signatures_to_gist(signatures, gist_id, 'generated_signatures.html')
+    print("Email signatures generated successfully!")
 
 if __name__ == "__main__":
     main()
